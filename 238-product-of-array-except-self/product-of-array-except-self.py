b2004e5c223e0d2b975a784d31510e7efd0297db
Shiven Singh <ss.shiven44@gmail.com>
2026-09-08 22:41:38 +0100

    Time: 35 ms (25.09%) | Memory: 27.8 MB (13.44%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,15 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left = [1]
+        left_array = [1] * len(nums)
+        right_array = [1] * len(nums)
+
         for i in range(1, len(nums)):
-            left.append(left[i-1] * nums[i-1])
-
-        right = [1] * len(nums)   # isme empty isliye nahi banayi kyuki isme right se value insert karni hai and append sirf left to right work karta hai. isliye pehle index ke saath banayi list(array)
-        for i in range(len(nums)-2, -1, -1):  # *len(nums) matlab ye value itni baar repeat karo i.e. len(nums)
-            right[i] = right[i+1] * nums[i+1]
-
-        output = []     #output = [0] * len(nums)  aise bhi kar sakte hai
+            left_array[i] = left_array[i-1] * nums[i-1]
+        
+        for i in range(len(nums)-2, -1, -1):
+            right_array[i] = right_array[i+1] * nums[i+1]
+        
+        output = []
         for i in range(len(nums)):
-            output.append(left[i] * right[i]) #but fir yaha  output[i] = left[i] * right[i]
-
+            output.append(left_array[i]*right_array[i])
         return output
+            
+            
+            
